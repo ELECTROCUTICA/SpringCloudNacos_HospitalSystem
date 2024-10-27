830f313f7f7d04e9b0158c2edf42586ad70b0647
season of yanhua <s1942902357@126.com>
2024-10-27 18:44:23 +0800

    version 1.4

--- HospitalSystem_Pojo/src/main/java/com/HospitalSystem_Pojo/Utils/ChatGPTAPI.java
+++ HospitalSystem_Pojo/src/main/java/com/HospitalSystem_Pojo/Utils/ChatGPTAPI.java
@@ -14,9 +14,9 @@ import java.util.concurrent.*;
 public class ChatGPTAPI {
 
     private static final String URL = "https://api.chatanywhere.tech/v1/chat/completions";
-    private static final String API_KEY = "******";
+    private static final String API_KEY = "sk-0FipaHgKpAuhVS0YV45vLDV6fsTDJ5k6ABxbFu7LXKLlLJNm";
 
-    public static String sendRequestToChatGPT(String message) throws ExecutionException, InterruptedException {
+    public static String sendRequestToChatGPT(String message) {
         ExecutorService threadPool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors() / 2,
                 Runtime.getRuntime().availableProcessors() / 2,
                 0L,
